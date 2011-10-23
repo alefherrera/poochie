c349d436807110374eb6413cdd663ff207ccb2fe
nacho692@gmail.com <nacho692@gmail.com@e0aba6ba-9169-5f27-2011-ebe5ff79c722>
2011-10-23 22:01:31 +0000

    empeze con estilos de mostrar threads

--- index.php
+++ index.php
@@ -1,5 +1,44 @@
-<?php include $_SERVER['DOCUMENT_ROOT'].'/poochie/template/header.php';
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/poochie/template/header.php';
 save_lastPage();
-include $_SERVER['DOCUMENT_ROOT'].'/poochie/template/footer.php'; 
 ?>
+<head>
+    <link rel="stylesheet" href="/poochie/template/styles/index_style.css" type="text/css" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<div id="index_main" style="border-style:solid">
+    <table class="thread">
+        <tr>
+            <td>
+                1
+            </td>
+            <td>
+                FLECHITAS
+            </td>
+            <td>
+                Hay vida en marte? estanffffsa afas fafqw fwqf qwfjmwieof noiew wif
+            </td>
+            <td>
+                <table>
+                    <tr>
+                        <td>
+                            10 comentarios
+                        </td>
+                        <td>
+                            5 visitas
+                        </td>
+                    </tr>
+                </table>
+            <td>
+
+    </table>
+</td>
+</tr>
+</table>
+</div>
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/poochie/template/footer.php';
+?>
+
+
 

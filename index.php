96e9090d4549fe41cf0a1954015ff2b06aa71a1e
nacho692@gmail.com <nacho692@gmail.com@e0aba6ba-9169-5f27-2011-ebe5ff79c722>
2011-10-24 00:11:44 +0000

    Index y estilos

--- index.php
+++ index.php
@@ -6,35 +6,82 @@ save_lastPage();
     <link rel="stylesheet" href="/poochie/template/styles/index_style.css" type="text/css" />
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
-<div id="index_main" style="border-style:solid">
+<div id="index_main">
     <table class="thread">
         <tr>
-            <td>
+            <td class="column">
                 1
             </td>
-            <td>
-                FLECHITAS
+            <td class="column">
+                <table>
+                    <tr>
+                        <td class="up_arrow">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="total" align="center" >
+                            3200
+                        </td>
+                    </tr>
+                    <tr>
+
+                        <td class="down_arrow">
+                        </td>
+                    </tr>
+
+                </table>
             </td>
             <td>
-                Hay vida en marte? estanffffsa afas fafqw fwqf qwfjmwieof noiew wif
-            </td>
-            <td>
+                <!--Tabla de titulo, fecha, informacion en egeneral -->
                 <table>
                     <tr>
                         <td>
-                            10 comentarios
+                            <table>
+                                <tr>
+                                    <td class="positive">
+                                        +32
+                                    </td>
+                                    <td>
+                                        /
+                                    </td>
+                                    <td class="negative">
+                                        -20
+                                    </td>
+                                </tr>
+                            </table>
                         </td>
+                    </tr>
+                    <tr>
+                        <td colspan="4" class="title">
+                            <a href="/poochie/index.php">"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</a>
+                        </td>
+                    </tr>
+                    <tr>
                         <td>
-                            5 visitas
+                            <table>
+                                <tr>
+                                    <td class="user">
+                                        usuario
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="general">
+                                        10 comentarios
+                                    </td>
+                                    <td class="general">
+                                        5 visitas
+                                    </td>
+                                    <td>                             <td class="general">
+                                        01/01/1995
+                                    </td>
+                                </tr>
+                            </table>
                         </td>
                     </tr>
                 </table>
-            <td>
-
+            </td>
+        </tr>
     </table>
-</td>
-</tr>
-</table>
 </div>
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/poochie/template/footer.php';

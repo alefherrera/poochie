654a465a50a1bb3de8d7d8af200d6e659ee8ef10
nacho692@gmail.com <nacho692@gmail.com@e0aba6ba-9169-5f27-2011-ebe5ff79c722>
2011-10-24 02:46:31 +0000

    showthread

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/poochie/template/header.php';
 save_lastPage();
 ?>
 <head>
-    <link rel="stylesheet" href="/poochie/template/styles/index_style.css" type="text/css" />
+    <link rel="stylesheet" href="/poochie/template/styles/thread_style.css" type="text/css" />
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
 <?php mostrar_titulo_thread();?>
